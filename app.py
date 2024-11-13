85a2d9a73bb40df8fe2ddb663fd1602b55005d17
oekoo <y.dykow22@gmail.com>
2024-11-13 14:59:06 +0100

    Check more credentials

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ load_dotenv()
 
 # Atlassian Key
 atlassian_key = os.getenv("ATLASSIAN_API")
+jira_user = os.getenv("JIRA_USER_EMAIL")
+jira_url = os.getenv("JIRA_URL")
 
+# Check if env file loaded correctly
 print("API Key loaded:", atlassian_key is not None)
+print("Jira User loaded:", jira_user is not None)
+print("Jira Domain loaded:", jira_url is not None)
