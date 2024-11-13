ef131cd0ba73c533fcd93b909988382d38223900
oekoo <y.dykow22@gmail.com>
2024-11-13 15:06:01 +0100

    Check API request for Test Ticket

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# pip3 install requests python-dotenv
+
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -12,3 +15,27 @@ jira_url = os.getenv("JIRA_URL")
 print("API Key loaded:", atlassian_key is not None)
 print("Jira User loaded:", jira_user is not None)
 print("Jira Domain loaded:", jira_url is not None)
+
+# Ticket ID
+ticket_id = "SCRUM-2"
+
+# URL for API-Request
+url = f"{jira_url}/rest/api/3/issue/{ticket_id}"
+
+# Header for API-Request
+headers = {
+    "Accept": "application/json"
+}
+
+auth = (jira_user, atlassian_key)
+
+# API-Request
+response = requests.get(url, headers=headers, auth=auth)
+
+# Check if request was successful
+if response.status_code == 200:
+    print("Request was successful")
+    print(response.json())
+else:
+    print("Request failed")
+    print(response.text)
